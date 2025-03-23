6963ee9c6c16386ed5266bd0a97cda95f8bbc315
ReneCarranza <renecarranza76@gmail.com>
2025-03-23 09:45:06 -0600

    Update 2023_10_19_212202_create_comentarios_table.php
    prueba

--- database/migrations/2023_10_19_212202_create_comentarios_table.php
+++ database/migrations/2023_10_19_212202_create_comentarios_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.gggggggrrrrrr
+     * Run the migrations.gggggggrrrrrrrr
      *
      * @return void
      */
